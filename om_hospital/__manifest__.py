4f34fdd8c726c55159ce6f8946b446f0896cfab1
abdosedroho <abdosedroho@gmail.com>
2021-07-13 03:46:02 +0200

    change version from 1 to 2

--- om_hospital/__manifest__.py
+++ om_hospital/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Hospital Management',
-    'version': '1.0',
+    'version': '2.0',
     'summary': 'Hospital Management Software',
     'sequence': -100,
     'description': """Hospital Management Software""",
